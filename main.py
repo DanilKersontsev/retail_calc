0d6e6aebbea9e5852bba9764b32e70f309dab196
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-03-31 10:00:42 +0300

    Arvutab esialgsest hinnast discount rate maha kui summa ületab 1000$ [delivers #181737680]

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ asukoht = input("Sisestage State: ").upper()
 
 esialgne_kauba_hind = kauba_kogus
 print( "kauba alghind on",esialgne_kauba_hind, "$",)
+
+if kauba_kogus >= 1000 and kauba_kogus < 5000:
+    discounted_hind =  kauba_kogus - (kauba_kogus * 0.03)
+    taxed_hind = (discounted_hind * float(asukoht) + discounted_hind)
+    print("Kogusumma koos soodustuse ja maksudega" ,taxed_hind, "$")
+
