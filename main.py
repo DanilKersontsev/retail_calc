594101a954c316224e293b9668459c6ca826decb
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-03-31 09:57:37 +0300

    Mina kui kasutaja näen väljastatud hinda ekraanil [delivers #181737659]

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("kui sooritate ostu mis on rohkem või võrdne 5000$ saate lisa soodustust
 kauba_valik = input("sisestage kaup mida soovite osta: ")
 kauba_kogus = int(input("Sisestage kauba kogus: "))
 asukoht = input("Sisestage State: ").upper()
+
+esialgne_kauba_hind = kauba_kogus
+print( "kauba alghind on",esialgne_kauba_hind, "$",)
